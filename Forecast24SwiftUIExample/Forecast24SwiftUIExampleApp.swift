ff8e8f009c248c8a17a8bcd3dc255ac979f81e89
Hoye Lam <hoyelam@protonmail.com>
2022-09-10 22:01:46 +0200

    Initial Commit

---
+++ Forecast24SwiftUIExample/Forecast24SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forecast24SwiftUIExampleApp.swift
+//  Forecast24SwiftUIExample
+//
+//  Created by Hoye Lam on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Forecast24SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Forecast24SwiftUIExampleUITests/Forecast24SwiftUIExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Forecast24SwiftUIExampleUITestsLaunchTests.swift
+//  Forecast24SwiftUIExampleUITests
+//
+//  Created by Hoye Lam on 10/09/2022.
+//
+
+import XCTest
+
+class Forecast24SwiftUIExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
